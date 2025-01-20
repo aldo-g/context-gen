a016e37542f00410a57e49193871bbcccf8aadd2
Alastair Grant <85125580+aldo-g@users.noreply.github.com>
2025-01-20 22:37:47 +0100

    black run

--- context_generator/cli.py
+++ context_generator/cli.py
@@ -17,7 +17,11 @@ DEFAULT_CONFIG: dict[str, Any] = {
         "package-lock.json",
         "LICENSE",
     ],
-    "exclude_paths": [".git", "__pycache__", "build"],  # Removed the empty string
+    "exclude_paths": [
+        ".git",
+        "__pycache__",
+        "build",
+    ],  # Removed the empty string
     "output_file": "file_context.txt",
     "exclude_hidden": True,  # New flag
 }
@@ -194,4 +198,3 @@ def main() -> None:
             exclude_paths=exclude_paths,
             exclude_hidden=exclude_hidden,
         )
-        
